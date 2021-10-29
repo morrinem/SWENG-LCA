f01431a54fd1f3b0daef8f3487637d68efb23f41
morrinem <91430602+morrinem@users.noreply.github.com>
2021-10-29 19:46:28 +0100

    updated LCA
    LCA now works for old tests with new array input system, but fails DAG specific tests

--- Java/LCA.java
+++ Java/LCA.java
@@ -6,12 +6,20 @@ public class LCA {
         if (root == null || a == null || b == null) return null;
         if (root.data == a.data || root.data == b.data) return root;
 
+        Node result = null;
         for (Node child : root.children)
         {
             Node current = findLCA(child, a, b);
-            if (current != null) return current;
+            if (result == null && current != null) result = current;
+            else if (current != null) result = root;
         }
-        return null;
+        return result;
+        //Node left = findLCA(root.left, a, b);
+        //Node right = findLCA(root.right, a, b);
+
+        //if (left != null && right != null) return root;
+        //if (left == null) return right;
+        //return left;
     }
 }
 
